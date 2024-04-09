61c40dffb55eff55810a4cfbd1bb575180ceb65c
Graysea91 <kennykidsjpjb@gmail.com>
2024-04-09 09:50:08 -0700

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    listing = my_list[:x]
+    i = 0
+    try:
+        for _ in listing:
+            i += 1
+        print("".join(map(str,listing)))
+        return i
+    except Exception as e:
+        pass
